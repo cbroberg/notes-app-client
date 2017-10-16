305752f9e6a39244597cb510c1aef45e57bcd89a
Christian Broberg <cb@webhouse.dk>
2017-10-16 14:05:16 +0200

    Holy Moly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
 
-render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
+registerServiceWorker();
