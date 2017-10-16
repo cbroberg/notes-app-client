92ec3ea36d5e8313803c6ab890692022117db9fc
Christian Broberg <cb@webhouse.dk>
2017-10-16 16:43:16 +0200

    Linting the crap out of it - code style updated

--- src/config.js
+++ src/config.js
@@ -14,21 +14,3 @@ export default {
 		IDENTITY_POOL_ID: "us-east-1:a256be78-fc35-46fb-9595-d147594af36b"
 	}
 }
-
-/* 
-
-apig-test \
---username='cb@webhouse.dk' \
---password='Passw0rd!' \
---user-pool-id='us-east-1_cayMvxHZ7' \
---app-client-id='1nsrjatuh22cdcttskcov7dd0r' \
---cognito-region='YOUR_COGNITO_REGION' \
---identity-pool-id='us-east-1:a256be78-fc35-46fb-9595-d147594af36b' \
---invoke-url='https://clpgpis47a.execute-api.us-east-1.amazonaws.com/prod' \
---api-gateway-region='us-east-1' \
---path-template='/notes' \
---method='POST' \
---body='{"content":"hello world","attachment":"hello.jpg"}'
-
-
-*/
