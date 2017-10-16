ad56944a0d7cb4496334f307888ab294c9148c0f
Christian Broberg <cb@webhouse.dk>
2017-10-16 14:29:36 +0200

    Hipster render use in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { render } from "react-dom"
 import { BrowserRouter as Router } from "react-router-dom"
 import App from "./App"
 import registerServiceWorker from "./registerServiceWorker"
 import "./index.css"
 
-ReactDOM.render(
+render(
 	<Router>
 		<App />
 	</Router>,
