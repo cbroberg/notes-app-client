d89b14e057efa722267c030163a588dd36b221b3
Christian Broberg <cb@webhouse.dk>
2017-10-17 17:05:36 +0200

    Testing build and deployment with Travis CI

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
 				<Navbar fluid collapseOnSelect>
 					<Navbar.Header>
 						<Navbar.Brand>
-							<Link to="/">ODEUM Notes</Link>
+							<Link to="/">ODEUM Notes (v. 0.1.0.)</Link>
 						</Navbar.Brand>
 						<Navbar.Toggle />
 					</Navbar.Header>
